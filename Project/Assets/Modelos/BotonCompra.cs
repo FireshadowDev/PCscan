65e519c60cd61cc43606395f9c31864d1ada87c3
FireshadowDev <fireshadow1997@gmail.com>
2026-03-15 21:55:50 +0100

    actualizando enlace web

--- Project/Assets/Modelos/BotonCompra.cs
+++ Project/Assets/Modelos/BotonCompra.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BotonCompra : MonoBehaviour{
 
     // URL de la tienda para el ratón ATK VXE R1 Pro
-    public string urlCompra = "https://vxe.com/products/vxe-dragonfly-r1-series-wireless-mouse?variant=47700096909526";
+    public string urlCompra = "https://fireshadowdev.github.io/PCscan/Web/";
 
     // Este método se activa al tocar el ratón en el móvil o hacer clic en el PC
     void OnMouseDown()
